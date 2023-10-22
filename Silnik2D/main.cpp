5d664cc9d0940b3b54b49294785acb916253f3cf
kardaw2513 <K.karolek643@gmail.com>
2023-10-22 15:00:08 +0200

    dodalem napis dla testu

--- Silnik2D/main.cpp
+++ Silnik2D/main.cpp
@@ -1,7 +1,10 @@
 ﻿#include "engine.h"
+using namespace std;
 
 int main() {
     Engine engine(800, 600); //singleton
+
+    cout << "dodalem to siema";
 
     if (engine.Init()) {
         engine.Run();
