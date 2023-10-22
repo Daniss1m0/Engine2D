02ce7f61457d93a852586065ac99ac41ac129b8f
Daniss1m0 <129661150+Daniss1m0@users.noreply.github.com>
2023-10-22 15:02:21 +0200

    Update main.cpp
    Ubralem to

--- Silnik2D/main.cpp
+++ Silnik2D/main.cpp
@@ -3,9 +3,7 @@ using namespace std;
 
 int main() {
     Engine engine(800, 600); //singleton
-
-    cout << "dodalem to siema";
-
+    
     if (engine.Init()) {
         engine.Run();
         engine.Deinit();
